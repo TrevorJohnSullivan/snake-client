dd184130c90d14b44f7b490aeb3639d73e91c991
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-14 11:35:32 -0330

    I added frame work for standard input and an escape function ctr-c

--- play.js
+++ play.js
@@ -1,2 +1,18 @@
-const {host, port} = require('./client');
+const { host, port } = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
